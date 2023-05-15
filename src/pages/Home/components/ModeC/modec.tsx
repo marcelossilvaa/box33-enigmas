9c461d3a4ca63361c973a362ca3558865279be87
Marcelo Silva <82605444+marcelossilvaa@users.noreply.github.com>
2023-05-15 14:56:31 -0300

    att

--- src/pages/Home/components/ModeC/modec.tsx
+++ src/pages/Home/components/ModeC/modec.tsx
@@ -4,9 +4,7 @@ export function ModeC() {
   return (
     <div className="flex justify-center items-center flex-col">
       <h1 className=" text-xl pt-8">Modo Campanha</h1>
-      <NavLink to="/campaignmode">
-        <a href="">Em breve...</a>
-      </NavLink>
+      <a href="">Em breve...</a>
     </div>
   );
 }
